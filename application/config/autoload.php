2ad0dea9dfbc2663325bf8051583f9aff5a4aaca
MwangiKevin <mwangikevinn@gmail.com>
2014-10-08 20:05:04 +0300

    installed mpdf

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','rb/rb','session','encrypt','PHPexcel/Classes/PHPexcel');
+$autoload['libraries'] = array('database','rb/rb','session','encrypt','PHPexcel/Classes/PHPexcel','mpdf');
 
 
 /*
