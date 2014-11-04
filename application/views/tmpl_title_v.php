be3fcd42642474c74a5dd5970f6d799f0d19a03b
Oscar Gichohi <shohiek@gmail.com>
2014-11-04 17:26:38 +0300

    accidentally deleted

---
+++ application/views/tmpl_title_v.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-6 head-title">
+		<div class="row" style="width:100%">
+			<div class="" style="width:20%;float:left;">
+				<img src="<?php echo base_url('assets/images/tz.png'); ?>" height="" width="" alt="" style="height:60px;width:60px;">
+			</div>
+			<div class="" style="width:60%;float:left;">
+				<h5>The United Republic of Tanzania</h5>
+			</div>
+			<div class="" style="width:20%;float:left;">
+				<img src="<?php echo base_url('assets/images/nacp.png'); ?>" height="" width="" alt="" style="height:60px;width:65px;">
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3"></div>
+</div>
